b388d4ff24fa04f4943e60021cdf07921b48c7f7
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-05 19:21:52 -0300

    Exercício

---
+++ CursoJS/aula08/test.js
@@ -0,0 +1,16 @@
+const nome = 'Gustavo';
+const sobrenome = 'Ferreira';
+const idade = 30;
+const peso = 84;
+const alturaEmMetro = 1.80;
+const anoAtual = 2024;
+let imc;
+let anoDeNascimento;
+
+// Calculo do IMC 
+imc = peso / (alturaEmMetro * alturaEmMetro);
+console.log('Peso IMC: ', imc)
+
+// Calculo do ano de nascimento
+anoDeNascimento = (anoAtual - idade);
+console.log("Ano de nascimento: ", anoDeNascimento)
