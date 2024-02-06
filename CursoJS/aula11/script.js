a886af203f1d843335d9d1231d3f58aef6111a77
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-06 19:39:14 -0300

    Update script.js

--- CursoJS/aula11/script.js
+++ CursoJS/aula11/script.js
@@ -1,3 +1,10 @@
-alert("Nova mensagem!")
-confirm("Deseja cancelar?")
-prompt("Pergunta")
+
+const primeiroNumero = prompt("Digite um número");
+primeiroNumero =  parseFloat(primeiroNumero);
+
+const segundoNumero = prompt("Digite mais um outr número:");
+segundoNumero = parseFloat(segundoNumero);
+
+const resultado = (primeiroNumero + segundoNumero);
+console.log(resultado)
+alert("O resultado da adição dos dois números digitas é: "+ resultado)
