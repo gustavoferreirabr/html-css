6cc91d5d41243b01a8bc369c0702048c76395ade
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-07 19:12:49 -0300

    Update script.js

--- CursoJS/aula11/script.js
+++ CursoJS/aula11/script.js
@@ -1,16 +1,7 @@
 let varA = 'A'; // B
 let varB = 'B'; // C
 let varC = 'C'; // A
-let varGeral = '';
 
-varGeral = varB;
-varA = varGeral;
+varA = varB;
 console.log(varA)
 
-varGeral = varC
-varB = varGeral;
-console.log(varB);
-
-varGeral = varA;
-varC = varGeral;
-console.log(varC)
