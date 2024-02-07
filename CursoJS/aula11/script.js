97c75cdf25eeec4e9740ce7c4c8017b930dd0913
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-07 19:20:41 -0300

    Update script.js

--- CursoJS/aula11/script.js
+++ CursoJS/aula11/script.js
@@ -5,7 +5,8 @@ let varC = 'C'; // A
 // Primeira opção:
 
 const varX = varA;
-console.log(varX)
+// Usei uma const, porque a const não podemos mudar o seu valor, sendo assim, atribui o valor da variável varA na minha const, como não poderia ser alterado esse valor, ficou como fixo e defino que o valor de varX é igual a VarA = A
+// console.log(varX)
 
 varA = varB
 console.log(varA);
