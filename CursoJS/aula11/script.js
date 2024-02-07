35dcc3ff981d0d0b25f9c6e2b866ada5dcba9052
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-07 19:18:05 -0300

    Update script.js

--- CursoJS/aula11/script.js
+++ CursoJS/aula11/script.js
@@ -2,6 +2,17 @@ let varA = 'A'; // B
 let varB = 'B'; // C
 let varC = 'C'; // A
 
-varA = varB;
-console.log(varA)
+// Primeira opção:
+
+const varX = varA;
+console.log(varX)
+
+varA = varB
+console.log(varA);
+
+varB = varC;
+console.log(varB);
+
+varC = varX;
+console.log(varC);
 
