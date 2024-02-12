2334a9de0cc20d45cc036bb9e02d59e15cd5fa64
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-12 13:22:31 -0300

    Update script.js

--- CursoJS/aula020/js/script.js
+++ CursoJS/aula020/js/script.js
@@ -1,8 +1,8 @@
 function meuEscopo(){
-    const form = document.querySelector('.form'); // Estou acessando no meu documento a minha tag form, a partir desse comando, posso modificar qualquer coisa dentro da minha tag form.
+    const form = document.querySelector('.form'); // Estou acessando os elementos que estão na minha tag form, a partir desse comando, posso modificar qualquer coisa dentro da minha tag form.
 
     function recebeEventoForm(evento){
-        evento.preventDefalt();
+        evento.preventDefalt();// Nessa linha, estou dizendo que o computador ira prevenir o evento que aconteceria quando o usuario enviar o o formulário.
 
         const nome = form.querySelector('.nome'); // Estou acessando a minha class nome....
         const sobrenome = form.querySelector('.sobrenome');
@@ -12,7 +12,11 @@ function meuEscopo(){
         console.log(nome.value, sobrenome, peso, altura);
     }
 
-    form.addEventListener('submit', recebeEventoForm);
+    form.addEventListener('submit', recebeEventoForm);//Neste caso, quando o usuário, cliclar no meu button, submit, era será executado a minha função evento.preventDefalt, no qual não vai realizar a função automatica do navegador.
 }
 
 meuEscopo();
+
+// Objetivo:
+
+// 
