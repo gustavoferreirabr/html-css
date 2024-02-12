d886ab141b0227bd6ae680ee75ecee5ef07fd409
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-12 13:16:06 -0300

    Update script.js

--- CursoJS/aula020/js/script.js
+++ CursoJS/aula020/js/script.js
@@ -1,10 +1,10 @@
 function meuEscopo(){
-    const form = document.querySelector('.form');
+    const form = document.querySelector('.form'); // Estou acessando no meu documento a minha tag form, a partir desse comando, posso modificar qualquer coisa dentro da minha tag form.
 
     function recebeEventoForm(evento){
         evento.preventDefalt();
 
-        const nome = form.querySelector('.nome');
+        const nome = form.querySelector('.nome'); // Estou acessando a minha class nome....
         const sobrenome = form.querySelector('.sobrenome');
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
