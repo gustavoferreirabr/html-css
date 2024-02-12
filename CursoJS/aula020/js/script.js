0836656477f59e41c3a231cb9c60207dc7c0b1a0
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-12 13:14:11 -0300

    Update script.js

--- CursoJS/aula020/js/script.js
+++ CursoJS/aula020/js/script.js
@@ -5,10 +5,14 @@ function meuEscopo(){
         evento.preventDefalt();
 
         const nome = form.querySelector('.nome');
-        const nome = form.querySelector('.nome');
-        const nome = form.querySelector('.nome');
-        const nome = form.querySelector('.nome');
-        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
 
+        console.log(nome, sobrenome, peso, altura);
     }
+
+    form.addEventListener('submit', recebeEventoForm);
 }
+
+meuEscopo();
