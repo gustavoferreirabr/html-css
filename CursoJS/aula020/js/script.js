991bac37c44c5cd1c6b56434e0a338a00a198c80
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-12 13:12:20 -0300

    .

---
+++ CursoJS/aula020/js/script.js
@@ -0,0 +1,10 @@
+function meuEscopo(){
+    const form = document.querySelector('.form');
+
+    function recebeEventoForm(evento){
+        evento.preventDefalt();
+
+        const nome = form.querySelector('.nome');
+        
+    }
+}
