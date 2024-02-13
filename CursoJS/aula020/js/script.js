a93e0a8a06aa7d9b45029d8ed4b610a00e5a5320
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-13 13:10:20 -0300

    Update script.js

--- CursoJS/aula020/js/script.js
+++ CursoJS/aula020/js/script.js
@@ -1,34 +1,28 @@
 function meuEscopo() {
-    const form = document.querySelector('.form'); // Estou acessando os elementos que estão na minha tag form, a partir desse comando, posso modificar qualquer coisa dentro da minha tag form.
-    let resultado = document.querySelector('.resultado'); // 
+    const form = document.querySelector('.form');
+    let resultado = document.querySelector('.resultado');  
     const pessoas = [];
-    function recebeEventoForm(evento) {
-        evento.preventDefalt();// Nessa linha, estou dizendo que o computador ira prevenir o evento que aconteceria quando o usuario enviar o o formulário.
 
-        const nome = form.querySelector('.nome'); // Estou acessando a minha class nome que está dentro do meu documento form.query.
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
         const sobrenome = form.querySelector('.sobrenome');
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
 
-
-        function criarObj(nome, sobrenome, peso, altura){
-            return {
-                nome: nome,
-                sobrenome: sobrenome,
-                peso: peso,
-                altura: altura
-            };
-       
-        }
-
         criarObj(nome.value, sobrenome.value, peso.value, altura.value);
 
+        function criarObj(nome, sobrenome, peso, altura){
+            pessoas.push = {nome, sobrenome, peso, altura}
+            console.log(pessoas);
+        }
 
         resultado.innerHTML = 
         `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}<p>`
     }
 
-    form.addEventListener('Submit', recebeEventoForm);//Neste caso, quando o usuário, cliclar no meu button, submit, era será executado a minha função evento.preventDefalt, no qual não vai realizar a função automatica do navegador.
+    form.addEventListener('submit', recebeEventoForm);
 }
 
 meuEscopo();
