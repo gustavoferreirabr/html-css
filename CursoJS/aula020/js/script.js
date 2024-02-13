e2d60654ebac8ee53814add2f47295ad91bafb7c
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-13 11:19:33 -0300

    Update script.js

--- CursoJS/aula020/js/script.js
+++ CursoJS/aula020/js/script.js
@@ -1,8 +1,8 @@
-function meuEscopo(){
+function meuEscopo() {
     const form = document.querySelector('.form'); // Estou acessando os elementos que estão na minha tag form, a partir desse comando, posso modificar qualquer coisa dentro da minha tag form.
-    const resultado = document.querySelector('.resultado'); // 
+    let resultado = document.querySelector('.resultado'); // 
     const pessoas = [];
-    function recebeEventoForm(evento){
+    function recebeEventoForm(evento) {
         evento.preventDefalt();// Nessa linha, estou dizendo que o computador ira prevenir o evento que aconteceria quando o usuario enviar o o formulário.
 
         const nome = form.querySelector('.nome'); // Estou acessando a minha class nome que está dentro do meu documento form.query.
@@ -10,9 +10,16 @@ function meuEscopo(){
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
 
-        console.log(nome.value, sobrenome, peso, altura);
+        pessoas.push({
 
-        pessoas = [nome.value, sobrenome.value, peso.value, altura.value]
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value,
+
+        })
+
+        console.log(pessoas)
 
     }
 
