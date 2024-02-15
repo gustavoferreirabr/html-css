d418f96fc08703a356540c029df738afb5862556
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-15 19:22:00 -0300

    Update main.js

--- CursoJS/aula026/modelo/js/main.js
+++ CursoJS/aula026/modelo/js/main.js
@@ -17,39 +17,23 @@ function meuEscopo(){
             return calculo;
         }
 
-        const resultado = calculoIMC(altura, peso);
-        // Entre 18,5 e 24,9 = Peso normal
-        // Entre 25 e 29,9 = Sobrepeso
-        // Entre 30 e 34,9 = Obsidade grau 1
-        // Entre 35 e 39,9 = Obesidade grau 2
-        // Mais do que 40 = Obseidade grau 3
-        
+        const resultado = calculoIMC(altura, peso);        
 
         if(resultado < 18.5){
-            output.innerHTML = "Abaixo do peso";
+            output.innerHTML = `Abaixo do peso (IMC ${resultado})`;
         } else if (resultado >= 18.5 && resultado <= 24.9){
-            output.innerHTML = "Peso normal"
+            output.innerHTML = `Peso normal (IMC ${resultado})`
         } else if (resultado >= 25 && resultado <= 29.9){
-            output.innerHTML = "Sobrepeso"
+            output.innerHTML = `Sobrepeso (IMC ${resultado})`
         } else if (resultado >= 30 && resultado <= 34.9){
-            output.innerHTML = "Obesidade grau 1"
+            output.innerHTML = `Obesidade grau 1 (IMC ${resultado})`
         } else if (resultado >= 35 && resultado <= 39.9){
-            output.innerHTML = "Obesidade grau 2"
+            output.innerHTML = `Obesidade grau 2 (IMC ${resultado})`
         } else {
-            output.innerHTML = "Obesidade grau 3"
+            output.innerHTML = `Obesidade grau 3 (IMC ${resultado})`
         }
 
     }
     
 }
 meuEscopo();
-
-// FEITO = Primeiro, devemos bloquear o evento do formulário, visto que se deixarmos o formulário agir da forma que o mesmo age, não iremos conseguir obter o resultado. 
-
-// FEITO = Segundo, devemos obter os dados que serão preenchidos no nossos inputs altura e largura.
-
-// FEITO Terceiro, devemos efetuar a divisão entre altura e input.
-
-    // Quarto, após realizar o calculo, devemos comparar o resultado com os indices e determina em condições se o usuario está acima ou abaixo do peso
-
-    // Depois disso, devemos exibir se o usuario está acima ou abaixo do peso. exibir embaixo do button, através do document.
