1d93dc26e0c5359b15f7219f73b62dd696f88f7e
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-15 18:15:56 -0300

    Update main.js

---
+++ CursoJS/aula026/modelo/js/main.js
@@ -0,0 +1,9 @@
+// Primeiro, devemos bloquear o evento do formulário, visto que se deixarmos o formulário agir da forma que o mesmo age, não iremos conseguir obter o resultado.
+
+// Segundo, devemos obter os dados que serão preenchidos no nossos inputs altura e largura.
+
+// Terceiro, devemos efetuar a divisão entre altura e input.
+
+// Quarto, após realizar o calculo, devemos comparar o resultado com os indices e determina em condições se o usuario está acima ou abaixo do peso
+
+// Depois disso, devemos exibir se o usuario está acima ou abaixo do peso.
