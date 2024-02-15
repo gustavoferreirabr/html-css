c0b8afb866aaf6ad5391118f8b3804945ce2ac11
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-15 18:20:10 -0300

    Update main.js

--- CursoJS/aula026/modelo/js/main.js
+++ CursoJS/aula026/modelo/js/main.js
@@ -1,9 +1,14 @@
-// Primeiro, devemos bloquear o evento do formulário, visto que se deixarmos o formulário agir da forma que o mesmo age, não iremos conseguir obter o resultado.
+function meuEscopo(){
 
-// Segundo, devemos obter os dados que serão preenchidos no nossos inputs altura e largura.
+    // Primeiro, devemos bloquear o evento do formulário, visto que se deixarmos o formulário agir da forma que o mesmo age, não iremos conseguir obter o resultado.
 
-// Terceiro, devemos efetuar a divisão entre altura e input.
+    // Segundo, devemos obter os dados que serão preenchidos no nossos inputs altura e largura.
 
-// Quarto, após realizar o calculo, devemos comparar o resultado com os indices e determina em condições se o usuario está acima ou abaixo do peso
+    // Terceiro, devemos efetuar a divisão entre altura e input.
 
-// Depois disso, devemos exibir se o usuario está acima ou abaixo do peso.
+    // Quarto, após realizar o calculo, devemos comparar o resultado com os indices e determina em condições se o usuario está acima ou abaixo do peso
+
+    // Depois disso, devemos exibir se o usuario está acima ou abaixo do peso. exibir embaixo do button, através do document.
+
+
+}
