456f2df36f098624f2a41a58d347a0919c3ee56c
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-17 11:35:06 -0300

    Update main.js

--- CursoJS/aula029/js/main.js
+++ CursoJS/aula029/js/main.js
@@ -4,7 +4,12 @@ let output = document.querySelector('.output');
 
 let data = new Date();
 const diaSemana = data.getDay();
-console.log(diaSemana)
+const diaMes = data.getDate();
+const mesNumber = data.getMonth() + 1;
+const ano = data.getFullYear();
+const hora = data.getHours();
+const minutos = data.getMinutes();
+console.log(minutos);
 
 const diaSemanaTexto = localizadorDiaSemana(diaSemana);
 function localizadorDiaSemana(diaSemana) {
@@ -45,5 +50,71 @@ function localizadorDiaSemana(diaSemana) {
     return diaSemanaText;
 }
 
+const mesTexto = localizadorMes(mesNumber);
+console.log(mesTexto)
+function localizadorMes(mesNumber) {
+
+    let mesPorEscrito;
+
+    switch (mesNumber) {
+        case 1:
+            mesPorEscrito = "Janeiro";
+            break;
+
+        case 2:
+            mesPorEscrito = "Fevereiro";
+            break;
+
+        case 3:
+            mesPorEscrito = "Março";
+            break;
+
+        case 4:
+            mesPorEscrito = "Abril";
+            break;
+
+        case 5:
+            mesPorEscrito = "Maio";
+            break;
+
+        case 6:
+            mesPorEscrito = "Junho";
+            break;
+
+        case 7:
+            mesPorEscrito = "Julho";
+            break;
+
+        case 8:
+            mesPorEscrito = "Agosto";
+            break;
+
+        case 9:
+            mesPorEscrito = "Setembro";
+            break;
+
+        case 10:
+            mesPorEscrito = "Outubro";
+            break;
+
+        case 11:
+            mesPorEscrito = "Novembro";
+            break;
+
+        case 12:
+            mesPorEscrito = "Dezembro";
+            break;
+
+        default:
+            mesPorEscrito = '';
+            break;
+    }
+
+    return mesPorEscrito;
+}
+
+
+
+
 console.log(diaSemanaTexto)
-output.innerHTML = "Dia da semana"+ diaSemanaTexto;
+output.innerHTML = `${diaSemanaTexto}, ${diaMes} de ${mesTexto} de ${ano}, ${hora}:${minutos}`
