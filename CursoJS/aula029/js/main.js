6b1133b0368b382cb6be43cc7525a72c20773520
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-17 10:52:24 -0300

    Update main.js

--- CursoJS/aula029/js/main.js
+++ CursoJS/aula029/js/main.js
@@ -2,22 +2,49 @@
 
 // Primeiro, devemos exibir o dia da semana.
 
-let data =  new Date();
+let data = new Date();
 const diaSemana = data.getDay();
 console.log(diaSemana)
 
 const diaSemanaTexto = localizadorDiaSemana(diaSemana);
-function localizadorDiaSemana(diaSemana){
+function localizadorDiaSemana(diaSemana) {
 
-    let diaSemanaTxt;
+    let diaSemanaText;
 
-    if(diaSemana === 0){
-       diaSemanaTxt = "Domingo";
-    } else if(diaSemana === 6){
-       diaSemanaTxt = "Sabado";
+    switch (diaSemana) {
+        case 0:
+            diaSemanaText = "Domingo";
+            break;
+
+        case 1:
+            diaSemanaText = "Segunda";
+            break;
+
+        case 2:
+            diaSemanaText = "Terça";
+            break;
+
+        case 3:
+            diaSemanaText = "Quarta";
+            break;
+        case 4:
+            diaSemanaText = "Quinta";
+            break;
+
+        case 5:
+            diaSemanaText = "Sexta";
+            break;
+
+        case 6:
+            diaSemanaText = "Sabado";
+            break;
+
+        default:
+            diaSemanaText = "Não foi encontrado o dia da semana";
+            break;
     }
 
-    return diaSemanaTxt;
+    return diaSemanaText;
 
 }
 
