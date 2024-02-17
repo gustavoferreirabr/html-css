ecd40e9c427a11660a06036dd7ca3c886a947831
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-17 10:29:44 -0300

    .

--- CursoJS/aula029/js/main.js
+++ CursoJS/aula029/js/main.js
@@ -1,1 +1,12 @@
-alert("Olá mundo!")
+// Exibir: (dia da semana), o (dia do mes) de (mês) de (ano), (horário:)
+
+
+let data = new Date('2003-05-13 03:05:44');
+console.log(data)
+console.log("Dia", data.getDate()); // Dia
+console.log("Mês", data.getMonth() + 1); // Mês - Inidice começa 0
+console.log("Ano", data.getFullYear());
+console.log("Horas", data.getHours());
+console.log("Minutos", data.getMinutes());
+console.log("Segundos", data.getSeconds());
+console.log("Dia da semana", data.getDay()); // o domingo começa em 0 e o sabado vai até 6
