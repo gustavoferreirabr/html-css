2285c59ce4fb6a875e1549c51da7f3a44bdd4c59
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-19 19:13:23 -0300

    Update script.js

--- CursoJS/aula032/script.js
+++ CursoJS/aula032/script.js
@@ -1,5 +1,5 @@
 //    indice:   0   1  2  3
-const numeros = [1, 2, 3, 4] // Esse comando (...resto), significa que ele ira pegar o resto dos elementos/indices que estão dentro da minha array.
+const numeros = [100, 200, 300, 400, 500] // Esse comando (...resto), significa que ele ira pegar o resto dos elementos/indices que estão dentro da minha array.
 const [primeiroIndice, segundoIndice, ...resto] = numeros; // Isso daqui apenas funciona com números, foi feito o teste.
 
 console.log(primeiroIndice, segundoIndice);
