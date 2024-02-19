e98560e9a7ae0d2137cf7f7286683c11cba5b4bc
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-19 19:09:32 -0300

    Update script.js

--- CursoJS/aula032/script.js
+++ CursoJS/aula032/script.js
@@ -1,5 +1,5 @@
 //    indice:   0   1  2  3
-const numeros = [A, B, C, D]
-let primeiroIndice, segundoIndice = numeros;
+const numeros = [1, 2, 3, 4]
+const [primeiroIndice, segundoIndice] = numeros; // Isso daqui apenas funciona com números, foi feito o teste.
 
 console.log(primeiroIndice, segundoIndice);
