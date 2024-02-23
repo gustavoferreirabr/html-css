7db9b500a8ef70ccfa64586571cd99ee541cd0fb
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-23 19:12:44 -0300

    Update main.js

--- CursoJS/aula035/modelo/js/main.js
+++ CursoJS/aula035/modelo/js/main.js
@@ -2,10 +2,10 @@ const elementos = [
     {tag: 'p', texto:'Olá mundo - Frase 1'},
     {tag: 'div', texto:'Olá Gustavo - Frase 2'},
     {tag: 'section', texto:'Olá cidade - Frase 3'},
-    {tag: 'footer', texto:'Ola gente! - Frase 4'}
+    {tag: 'footer', texto:'Ola gente! - Frase 4'},
 ]
 
-const container = document.querySelector('container');
+const container = document.querySelector('.container');
 const div = document.createElement('div');
 
 for(let i = 0; i < elementos.length; i++){
