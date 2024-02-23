4062c22ea3f5b0bbb2454e3857777b1fdaee6ccc
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-23 17:44:50 -0300

    Update main.js

--- CursoJS/aula035/modelo/js/main.js
+++ CursoJS/aula035/modelo/js/main.js
@@ -1,6 +1,6 @@
 let msg = document.querySelector('.paragrafo');
 msg.innerHTML = '';
 
-for(let i = 1; i < 4; i++){
-    msg += `Frase ${i} <br>`
+for(let i = 1; i <= 4; i++){
+    msg.innerHTML += `Frase ${i} <br>`;
 }
