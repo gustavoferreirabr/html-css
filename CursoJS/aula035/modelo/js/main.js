57f23a1edf9f8d878ce2eb8956c6ab9f96b364ba
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-23 19:11:42 -0300

    Update main.js

--- CursoJS/aula035/modelo/js/main.js
+++ CursoJS/aula035/modelo/js/main.js
@@ -5,8 +5,8 @@ const elementos = [
     {tag: 'footer', texto:'Ola gente! - Frase 4'}
 ]
 
-let container = document.querySelector('container');
-let div = document.createElement('div');
+const container = document.querySelector('container');
+const div = document.createElement('div');
 
 for(let i = 0; i < elementos.length; i++){
 
