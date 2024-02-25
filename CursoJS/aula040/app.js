34b549db35ca323ddf6d02991be299a80bd6976b
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-25 16:08:41 -0300

    Update app.js

--- CursoJS/aula040/app.js
+++ CursoJS/aula040/app.js
@@ -2,3 +2,14 @@
 // Devemos identificar se realmente o numero é um numero e em caso seja confirmado, devemos retornar esse valor.
 // Devemos identificar se esse numero é de 0 a 100, se for false, devemos falar para para de executar o código e seguir novamente para inserir o numero correto.
 // Depois, devemos realizar as condicionais
+
+function meuEscopo(numeroDado){
+
+   if (numeroDado >= 0 && numeroDado <= 100){
+     console.log("Numero Valido") 
+
+   } else console.log("Digite um numero valido de 0 a 100")
+
+}
+
+console.log(meuEscopo(Number(prompt("Digite um numero"))));
