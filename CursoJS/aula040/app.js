cffb4c7f0671d7e38e44d4e4d13cef1dfb05c19d
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-25 15:49:39 -0300

    .

---
+++ CursoJS/aula040/app.js
@@ -0,0 +1,4 @@
+// Devemos receber um numero ou valor
+// Devemos identificar se realmente o numero é um numero e em caso seja confirmado, devemos retornar esse valor.
+// Devemos identificar se esse numero é de 0 a 100, se for false, devemos falar para para de executar o código e seguir novamente para inserir o numero correto.
+// Depois, devemos realizar as condicionais
