6fd1af3466d866e7e1f8fbae2779a5c49003d3ed
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-27 16:53:55 -0300

    .

--- CursoJS/aula043/modelo/js/main.js
+++ CursoJS/aula043/modelo/js/main.js
@@ -1,1 +1,8 @@
-alert("Olá mundo!")
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar')
+
+iniciar.addEventListener('click', function(event) {
+    alert('Cliquei no inciar')
+})
