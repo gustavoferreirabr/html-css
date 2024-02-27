1e79941877db7c24adb652d5ccb2972fbf5a0402
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-27 16:36:06 -0300

    Update app.js

--- CursoJS/aula042/app.js
+++ CursoJS/aula042/app.js
@@ -4,9 +4,17 @@ function mostraHora(){
     return data.toLocaleTimeString('pt-BR', {
         hour12: false
     });
-
 }
 
 const cronometro = setInterval(function(){
     console.log(mostraHora());
 }, 1000)
+
+
+const duracao = setTimeout(function(){
+    clearInterval(cronometro);
+}, 10000)
+
+const mensagemAgendada = setTimeout(function(){
+    console.log("Olá, descreva sua solicitação:")
+}, 15000)
