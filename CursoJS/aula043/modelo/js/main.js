3655acac35d85991231462fd7d85b22700d3c540
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-02-29 21:32:55 -0300

    Update main.js

--- CursoJS/aula043/modelo/js/main.js
+++ CursoJS/aula043/modelo/js/main.js
@@ -1,16 +1,3 @@
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar')
-
-iniciar.addEventListener('click', function(event) {
-    alert('Cliquei no inciar')
-})
-
-pausar.addEventListener('click', function(event){
-
-})
-
-zerar.addEventListener('click', function(event){
-    
-})
+const pausar = document.qu
