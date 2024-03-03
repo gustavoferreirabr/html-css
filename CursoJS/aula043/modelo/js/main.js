9cdd397736adb9f53959ef31974e1bda404d84bf
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-03-02 22:14:10 -0300

    Update main.js

--- CursoJS/aula043/modelo/js/main.js
+++ CursoJS/aula043/modelo/js/main.js
@@ -1,4 +1,32 @@
-nction(event){
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar')
+
+function mostraHora(){
+
+    let data = new Date();
+    return data.toLocaleTimeString('pt-BR', {
+       
+    });
+
+}
+
+iniciar.addEventListener('click', function(event) {
+    
+    const cronometro = setInterval(function(){
+        return console.log(mostraHora());
+    }, 1000)
+
+    relogio.innerHTML = cronometro;
+
+})
+
+pausar.addEventListener('click', function(event){
+
+})
+
+zerar.addEventListener('click', function(event){
     
 })
 
