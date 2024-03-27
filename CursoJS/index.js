decc1ff2706fa59f7a8171c8a2d4f1dc9b20dc8a
Gustavo Ferreira <125163446+gustavoferreirabr@users.noreply.github.com>
2024-03-26 23:30:43 -0300

    Update index.js

--- CursoJS/index.js
+++ CursoJS/index.js
@@ -3,4 +3,16 @@ const canal_2 = 2
 const canal_3 = 3
 const canal_4 = 4
 
-let canal_digitado_usuario = Number
+let canal_digitado_usuario = Number(prompt("Digite um numero de canal"));
+
+if(canal_digitado_usuario == canal_1){
+    console.log("Futebol");
+} else if(canal_digitado_usuario == canal_2){
+    console.log("Jornal")
+} else if(canal_digitado_usuario == canal_3){
+    console.log("Politica")
+} else if(canal_digitado_usuario == canal_4){
+    console.log("Desenho")
+} else{
+    console.log("O canal digitado não está disponível")
+}
